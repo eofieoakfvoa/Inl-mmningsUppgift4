cb4cdc7cfb07f9b649b982bfcbf534828915bb27
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-04-13 19:51:02 +0200

    :cat:

--- Del1.py
+++ Del1.py
@@ -3,8 +3,11 @@ import math
 class BaseShape:
     def __init__(self):
         self._Area = None
+        self.CalculateArea() # eftersom __Init__ kallas EFTER objektet redan existerar betyder att det som inheritar detta kommer redan att ha overridat CalculateArea()
+        # och då kommer detta använda dens version. är ganska glad att jag kom på detta, dock betyder det att super() kommer behövas vara sist, och det kanske pånågot sätt kan skapa problem
+
     def CalculateArea(): #abstrakt även fast jag tror att python inte direkt har type safety och man nog inte skulle behöva att verifiera att den har CalculateArea() 
-        pass
+        print("ANVÄND INTE BASESHAPE KLASSEN!!!!! ifall du inte gjorde det vet jag inte hur du lyckades")
 
     def SetArea(self, newArea):
         if not self._IsValidNumber(newArea):
@@ -18,45 +21,52 @@ class BaseShape:
 
 class Rectangle(BaseShape):
     def __init__(self, base, height): # basically en constructor, gör mycket av vad en construktor gör men by defenition är inte en constructor där __new__ värkar vara mer som en construktor :p
-        super().__init__() # super() gör så att BaseShapes __Init__ också kallas annars så hade denna __init__ overrida BaseShape __init__ 
         self.Base = base
         self.Height = height
+        super().__init__() # super() gör så att BaseShapes __Init__ också kallas annars så hade denna __init__ overrida BaseShape __init__ 
     def CalculateArea(self):
         self.SetArea(self.Base * self.Height)
 
 class Square(BaseShape):
     def __init__(self, base, height): 
-        super().__init__()
         self.Base = base
         self.Height = height
+        super().__init__()
     def CalculateArea(self):
         self.SetArea(self.Base * self.Height)
 
 class Triangle(BaseShape):
     def __init__(self, base, height): 
-        super().__init__()
         self.Base = base
         self.Height = height
+        super().__init__()
     def CalculateArea(self):
         self.SetArea((self.Base * self.Height) / 2)
 
 class Circle(BaseShape):
     def __init__(self, radius): 
-        super().__init__()
         self.Radius = radius
+        super().__init__()
     def CalculateArea(self):
         self.SetArea(math.pi * self.Radius**2)
 
 class Hexagon(BaseShape):
     def __init__(self, sidelength):  
-        super().__init__()
         self.SideLength = sidelength
+        super().__init__()
     def CalculateArea(self):
         self.SetArea((3 * math.sqrt(3) * (self.SideLength ** 2)) / 2) #https://how.dev/answers/how-to-get-the-area-of-a-hexagon-in-python
 
 
 
-hello = Hexagon(5)
-hello.CalculateArea()
-print(hello.GetArea())
-input()
+testHexagon = Hexagon(5)
+testCircle = Circle(5)
+testTriangle = Triangle(5, 5)
+testSquare = Square(5,5)
+testRectangle = Rectangle(5,5)
+print("Areor: ",
+      f"\n Kvadrat : {testSquare.GetArea()} , bas : {testSquare.Base} , höjd , {testSquare.Height}",
+      f"\n Rektangle : {testRectangle.GetArea()} , bas : {testRectangle.Base} , höjd , {testRectangle.Height}",
+      f"\n Triangle : {testTriangle.GetArea()} , bas : {testTriangle.Base} , höjd , {testTriangle.Height}",
+      f"\n Cirkel : {testCircle.GetArea()} , radien : {testCircle.Radius}",
+      f"\n Hexagon : {testHexagon.GetArea()} , sidlängd : {testHexagon.SideLength}")
