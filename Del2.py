86e453638b398e58dd814e33e824baab7f37854a
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-04-14 01:07:57 +0200

    :cat:

--- Del2.py
+++ Del2.py
@@ -1,16 +1,28 @@
 import turtle
 from enum import Enum
 def main():
-    flagCanvasSize = [500, 500] 
+    MaxFlagSize = [150 * Sweden.flagRatio, 150]
+    print(MaxFlagSize)
+    gridSize = [MaxFlagSize[0] / Sweden.flagWidth, MaxFlagSize[1] / Sweden.flagWidth]
     Screen = turtle.getscreen()
     Cursor = turtle.Turtle()
+    Cursor.goto(-MaxFlagSize[0] / 2, MaxFlagSize[1] / 2)
     for row in Sweden.flagArray2D:
         for index in row:
-            color = Sweden.flagColorDictionary[index]
+            color = Sweden.flagColorDictionary[index].value
+            FillCube(Cursor, gridSize[0], gridSize[1], 0, 0, color)
     turtle.done()
 
-
-
+def FillCube(cursor ,Height, Width, StartX, StartY, Color):
+    print(Color)
+    cursor.fd(Width)
+    cursor.rt(90)
+    cursor.fd(Height)
+    cursor.rt(90)
+    cursor.fd(Width)
+    cursor.rt(90)
+    cursor.fd(Height)
+    cursor.rt(90)
 
 
 
@@ -21,11 +33,8 @@ def main():
 class Color(Enum):
     Yellow = "Yellow"
     Blue = "Blue"
-class BaseFlag:
-    flagArray2D = None
-    flagColorDictionary = None
 
-class Sweden(BaseFlag):
+class Sweden():
     flagArray2D = [
         [1,1,1,1,2,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,2,1,1,1,1,1,1,1,1,1],
@@ -35,7 +44,9 @@ class Sweden(BaseFlag):
         [1,1,1,1,2,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,2,1,1,1,1,1,1,1,1,1]]
     flagColorDictionary = {1 : Color.Blue, 2 : Color.Yellow}
-    
+    flagWidth =  len(flagArray2D[0])
+    flagHeight = len(flagArray2D)
+    flagRatio = flagWidth / flagHeight
     
 
 
