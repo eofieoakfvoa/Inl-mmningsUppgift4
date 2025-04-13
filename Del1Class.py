ac251178673d6ab08157044966da0b7644fd4052
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-04-13 19:04:25 +0200

    :cat:

--- Del1Class.py
+++ Del1Class.py
@@ -1,10 +1,31 @@
 class BaseShape:
-    Area = None
-    def CalculateArea():
+    def __init__(self):
+        self._Area = None
+    def CalculateArea(): #abstrakt även fast jag tror att python inte direkt har type safety och man nog inte skulle behöva att verifiera att den har CalculateArea() 
         pass
 
+    def SetArea(self, NewArea):
+        if not _IsValidNumber():
+            return
+        self._Area = NewArea
+    def GetArea(self):
+        return self._Area
+    def _IsValidNumber(Number):
+        if type(Number) != float:
+            return False
+        return True
+
 class Rectangle(BaseShape):
-    def __init__(self, base, height): # basically en constructor, värkar som att många på internet blir arga ifall man säger att det är en construktor
+    def __init__(self, base, height): # basically en constructor, gör mycket av vad en construktor gör men by defenition är inte en constructor där __new__ värkar vara mer som en construktor :p
+        super().__init__() # super() gör så att BaseShapes __Init__ också kallas annars så hade denna __init__ overrida BaseShape __init__ 
         self.Base = base
         self.Height = height
-    
+    def CalculateArea(self):
+        self._Area = self.Base * self.Height
+    def GetArea(self):
+        return self._Area
+
+hello = Rectangle(3, 5)
+hello.CalculateArea()
+print(hello.GetArea())
+input()
