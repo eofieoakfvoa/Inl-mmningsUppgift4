9f38700c65a86eafce25ff127de5103322ad406e
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-04-14 00:16:44 +0200

    :cat:

---
+++ Del2.py
@@ -0,0 +1,40 @@
+import turtle
+from enum import Enum
+def main():
+    Screen = turtle.getscreen()
+    Cursor = turtle.Turtle()
+    turtle.done()
+
+
+
+
+
+
+
+
+#classes
+#https://docs.python.org/3/library/enum.html
+class Color(Enum):
+    Yellow = "Yellow"
+    Blue = "Blue"
+class baseFlag:
+    flagArray2D = None
+    flagColorDictionary = None
+
+class Sweden(baseFlag):
+    flagArray2D = {
+        {1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+        {2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+        {1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+        {1,1,1,1,2,1,1,1,1,1,1,1,1,1},}
+    
+    
+
+
+
+
+
+main()
